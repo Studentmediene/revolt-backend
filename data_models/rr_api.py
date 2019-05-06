fa1985378d4f9dda5e1fcf19960af5b82802c461
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2019-05-06 21:39:33 +0200

    Don't evaluate API settings at startup
    This way, test code may alter the settings after-the-fact,
    if desired later.

--- data_models/rr_api.py
+++ data_models/rr_api.py
@@ -5,12 +5,9 @@ from django.conf import settings
 
 log = logging.getLogger(__name__)
 
-RR_API_BASE = settings.RR_API_BASE
-PODCAST_API_BASE = settings.PODCAST_API_BASE
-
 
 def get_shows():
-    r = requests.get('{base}/programmer/list'.format(base=RR_API_BASE), timeout=29.)
+    r = requests.get('{base}/programmer/list'.format(base=settings.RR_API_BASE), timeout=29.)
     r.raise_for_status()
 
     return r.json()
@@ -21,7 +18,7 @@ def get_podcast_url_from_digas_id(digas_id):
         # Short-circuit when no ID exists
         return None
 
-    url = '{base}/url/{digas_id}'.format(base=PODCAST_API_BASE, digas_id=digas_id)
+    url = '{base}/url/{digas_id}'.format(base=settings.PODCAST_API_BASE, digas_id=digas_id)
     r = requests.get(url, timeout=5.)
     if r.status_code == 404:
         # 404 is used to indicate that the ID was not found
